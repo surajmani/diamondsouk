300034a22ec147f560cb9e841a76525713a3ab50
HP <HP@DESKTOP-205R35M>
2018-02-06 15:54:01 +0530

    win add

---
+++ testRun/diamondsouk/platforms/windows/www/cordova_plugins.js
@@ -0,0 +1,85 @@
+﻿cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-appsee.Appsee",
+        "file": "plugins/cordova-plugin-appsee/www/Appsee.js",
+        "pluginId": "cordova-plugin-appsee",
+        "clobbers": [
+            "Appsee"
+        ]
+    },
+    {
+        "id": "cordova-plugin-console.logger",
+        "file": "plugins/cordova-plugin-console/www/logger.js",
+        "pluginId": "cordova-plugin-console",
+        "clobbers": [
+            "cordova.logger"
+        ]
+    },
+    {
+        "id": "cordova-plugin-console.console",
+        "file": "plugins/cordova-plugin-console/www/console-via-logger.js",
+        "pluginId": "cordova-plugin-console",
+        "clobbers": [
+            "console"
+        ]
+    },
+    {
+        "id": "cordova-plugin-dialogs.notification",
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "id": "cordova-plugin-dialogs.NotificationProxy",
+        "file": "plugins/cordova-plugin-dialogs/src/windows/NotificationProxy.js",
+        "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+            ""
+        ]
+    },
+    {
+        "id": "cordova-plugin-statusbar.statusbar",
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "id": "cordova-plugin-statusbar.StatusBarProxy",
+        "file": "plugins/cordova-plugin-statusbar/src/windows/StatusBarProxy.js",
+        "pluginId": "cordova-plugin-statusbar",
+        "runs": true
+    },
+    {
+        "id": "phonegap-plugin-push.PushNotification",
+        "file": "plugins/phonegap-plugin-push/www/push.js",
+        "pluginId": "phonegap-plugin-push",
+        "clobbers": [
+            "PushNotification"
+        ]
+    },
+    {
+        "id": "phonegap-plugin-push.PushPlugin",
+        "file": "plugins/phonegap-plugin-push/src/windows/PushPluginProxy.js",
+        "pluginId": "phonegap-plugin-push",
+        "merges": [
+            ""
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-appsee": "2.3.65",
+    "cordova-plugin-console": "1.0.7",
+    "cordova-plugin-dialogs": "1.2.1",
+    "cordova-plugin-statusbar": "2.1.3",
+    "cordova-plugin-whitelist": "1.2.2",
+    "phonegap-plugin-push": "1.8.4"
+};
+// BOTTOM OF METADATA
+});
---
+++ testRun/diamondsouk/platforms/windows/www/js/site.js
@@ -0,0 +1,317 @@
+﻿
+var baseUrl = 'https://www.diamondsouk.ae/api/';
+var Common = {
+    backBottom: function () {
+        var div = document.createElement('div');
+        div.onclick = function () {
+            sendBackScreen();
+        };
+        div.className = "backBottom";
+        div.innerHTML = '<i class="arrow left"></i><span> Back</span>';
+        $('.bg').prepend(div);
+    },
+    beforeSend: function () {
+        var header = {};
+        header.Authorization = 'Bearer ' + login.logData.ClientSecret;
+        return header;
+    },
+    Post: function (dataTopost, url, callBack, showprg) {
+        $.support.cors = true;
+        if (showprg == null || showprg == true) {
+            Common.ShwoprogressModal();
+        }
+        $.ajax({
+            type: "POST",
+            crossDomain: true,
+            url: baseUrl + '' + url,
+            contentType: 'application/json',
+            data: JSON.stringify(dataTopost),
+            headers: this.beforeSend(),
+            success: function (result) {
+                if (showprg == null || showprg == true) {
+                    Common.RemoveModal();
+                }
+                callBack(true, result);
+            },
+            error: function (result) {
+                if (showprg == null || showprg == true) {
+                    Common.RemoveModal();
+                }
+                if (result.status == 0) {
+                    callback(false, result.statusText);
+                    Common.showError("No internet, please check your internet connection");
+                    return;
+                }
+                try {
+                    callBack(false, JSON.parse(result.responseText));
+                } catch (e) {
+
+                    callBack(false, "");
+                }
+                try {
+                    var err = JSON.parse(result.responseText);
+                    Common.showError(Common.escapeRegExp(JSON.stringify(err.errors)));
+                } catch (e) {
+                    Common.showError(result.statusText);
+                }
+            }
+        });
+    },
+    Get: function (url, callback, showprg) {
+        if (showprg == null || showprg == true) {
+            Common.ShwoprogressModal();
+        };
+        $.ajax({
+            type: "GET",
+            crossDomain: true,
+            url: baseUrl + '' + url,
+            contentType: 'application/json',
+            headers: this.beforeSend(),
+            success: function (result) {
+                if (showprg == null || showprg == true) {
+                    Common.RemoveModal();
+                };
+                callback(true, result);
+            },
+            error: function (result) {
+                console.log(result);
+
+                if (showprg == null || showprg == true) {
+                    Common.RemoveModal();
+                }
+                if (result.status == 0) {
+                    callback(false, result.statusText);
+                    Common.showError("No internet, please check your internet connection");
+                    return;
+                }
+                try {
+                    callback(false, JSON.parse(result.responseText));
+                } catch (e) {
+                    callback(false, result.responseText);
+                }
+                try {
+                    var err = JSON.parse(result.responseText);
+                    Common.showError(Common.escapeRegExp(JSON.stringify(err.errors)));
+                } catch (e) {
+                    Common.showError(result.statusText);
+                }
+            }
+        });
+    },
+    LoadPage: function (url, callBack) {
+
+        Common.ShwoprogressModal();
+        $.ajax({
+            type: "GET",
+            url: url,
+            success: function (result) {
+                Common.RemoveModal();
+                callBack(true, result);
+            },
+            error: function (result) {
+                Common.RemoveModal();
+                callBack(false, JSON.parse(result.responseText));
+            }
+        });
+    },
+    ShwoprogressModal: function () {
+        try {
+            var mDocument = document.getElementsByClassName('bg')[0];
+            document.getElementById('openModalProgress').style.width = mDocument.clientWidth + 'px';
+            document.getElementById('openModalProgress').style.left = mDocument.offsetLeft + 'px';
+            $('#openModalProgress').addClass('showModal');
+        } catch (e) { }
+    },
+    RemoveModal: function () {
+        $('#openModalProgress').removeClass('showModal');
+    },
+    showError: function (str) {
+        mAlert.Ini(mAlert.Type.ERROR, 300, 180, 'ERROR', str, null);
+
+    },
+    showInfo: function (str, callback) {
+        mAlert.Ini(mAlert.Type.INFO, 300, 180, 'INFO', str, callback);
+    },
+    showInfoOkOnly: function (str, callback) {
+        mAlert.Ini(mAlert.Type.ERROR, 300, 180, 'INFO', str, callback);
+    },
+
+    showConfirmDialog: function (str, callBack, noCallBack) {
+        mAlert.Ini(mAlert.Type.YESNO, 300, 180, 'INFO', str, callBack, noCallBack);
+    }
+    ,
+    escapeRegExp: function (tr) {
+        return tr.replace(/"/g, '').replace(/{/g, '').replace(/}/g, '').replace(/\[/g, '').replace(/]/g, '').replace(/:/g, ' ');
+    },
+    SetJsonToLocal: function (key, Json) {
+        localStorage.setItem(key, JSON.stringify(Json));
+    },
+    GetJsonFromLocal: function (key) {
+        return JSON.parse(localStorage.getItem(key));
+    },
+    initializeSite: function () {
+        localStorage.removeItem("ChorusUserDetail");
+        localStorage.removeItem("chorusSives");
+    },
+    PrintInnerHtml: function (ele, data) {
+        $(ele).html(data);
+    },
+    GetCustomer: function () {
+        login.logData = JSON.parse(this.GetJsonFromLocal("ChorusUserDetail"));
+        if (login.logData == null) {
+            window.location.href = "index.html";
+        }
+
+    },
+    GetSieves: function () {
+        if (login.logData.ClientSecret == "") {
+            return;
+        }
+        var sieves = this.GetJsonFromLocal("chorusSives");
+        if (sieves == null) {
+            this.Get("products/get_product_sieve_ratio", function (s, r) {
+                Common.SetJsonToLocal("chorusSives", JSON.stringify(r));
+                sieves = JSON.parse(Common.GetJsonFromLocal("chorusSives"));
+
+                return sieves;
+            }, false);
+        }
+        else {
+            return JSON.parse(sieves);
+        }
+    },
+    GetValueFromObject: function (obj, k) {
+        var rtn = null;
+        Object.keys(obj).forEach(function (key) {
+            if (k.toLowerCase() == key.toLowerCase()) {
+                rtn = obj[key]
+            }
+        });
+        return rtn;
+    },
+    removeFromArray: function (arrOriginal, elementToRemove) {
+        return arrOriginal.filter(function (el) { return el !== elementToRemove });
+    },
+    AutoComplete: function (elem, autoPopdata, callback) {
+        var id = "autoPop" + new Date().getMilliseconds();
+        var div = document.createElement("div");
+        div.id = id;
+        div.style.display = "none";
+        elem.setAttribute("autoPopID", id);
+        div.className = "autoPop";
+        //div.style.top = elem.top  + "px";
+
+        div.style.width = (elem.offsetWidth - 10) + "px";
+        elem.parentNode.appendChild(div);
+        elem.onkeyup = (function () {
+            var autoPopID = elem.getAttribute("autoPopID");
+            div.innerHTML = "";
+            for (s in autoPopdata) {
+                if (autoPopdata[s].Name.toLowerCase().indexOf(this.value.toLowerCase()) > -1) {
+                    var option = document.createElement("div");
+                    option.innerHTML = autoPopdata[s].Name;
+                    option.setAttribute("val", autoPopdata[s].id);
+                    div.appendChild(option);
+                    option.onclick = function () {
+                        elem.value = this.innerHTML;
+                        callback({ value: this.getAttribute("val"), text: this.innerHTML });
+                        div.innerHTML = "";
+                        document.getElementById(autoPopID).style.display = "none";
+                    }
+                }
+            }
+            document.getElementById(autoPopID).style.display = "block";
+
+            //div.style.top = elem.top + elem.offsetHeight + "px";
+        });
+
+    }
+    , LoadUserImage: function (elem) {
+
+    }
+}
+
+
+var app = {
+    // Application Constructor
+    initialize: function () {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function () {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function () {
+        console.log('Received Device Ready Event');
+        console.log('calling setup push');
+        app.setupPush();
+    },
+    setupPush: function () {
+        console.log('calling push init');
+        var push = PushNotification.init({
+            "android": {
+                "senderID": "956859149736"
+            },
+            "browser": {},
+            "ios": {
+                "sound": true,
+                "vibration": true,
+                "badge": true
+            },
+            "windows": {}
+        });
+        console.log('after init');
+
+        push.on('registration', function (data) {
+            console.log('registration event: ' + data.registrationId);
+
+            var oldRegId = localStorage.getItem('registrationId');
+            if (oldRegId !== data.registrationId) {
+                // Save new registration ID
+                localStorage.setItem('registrationId', data.registrationId);
+                // Post registrationId to your app server as the value has changed
+            }
+
+            var parentElement = document.getElementById('registration');
+            var listeningElement = parentElement.querySelector('.waiting');
+            var receivedElement = parentElement.querySelector('.received');
+
+            listeningElement.setAttribute('style', 'display:none;');
+            receivedElement.setAttribute('style', 'display:block;');
+        });
+
+        push.on('error', function (e) {
+            console.log("push error = " + e.message);
+        });
+
+        push.on('notification', function (data) {
+            console.log('notification event');
+            navigator.notification.alert(
+                data.message,         // message
+                null,                 // callback
+                data.title,           // title
+                'Ok'                  // buttonName
+            );
+        });
+    }
+};
+
+
+//$(document).ready(function () {
+//    $('input').on('focus', function () {
+//        document.body.scrollTop = $(this).offset().top - 30;
+//    });
+//});
+
+
+
+
+
+
